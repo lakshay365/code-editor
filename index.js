bb25c6d50adaeac8aa9fa8a90d9a1e30f0285209
Lakshay Mehra <lakshay344@gmail.com>
2018-05-26 12:51:41 +0530

    Enable cors

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const filenameRequired = (req, res, next) => {
   if (!req.query.filename) return res.send({ error: 'Filename required!' })
   else next()
